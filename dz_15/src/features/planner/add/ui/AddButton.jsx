9a30d8a015f8099730bcbc933f24e403656727af
Dmytro Bihun <89932822+DmitriyBihun@users.noreply.github.com>
2026-02-02 11:45:22 +0200

    Delete dz_15 directory

--- dz_15/src/features/planner/add/ui/AddButton.jsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router"
-import style from './AddButton.module.css'
-
-function AddButton() {
-  return <Link to="/planner/new" className={style.addBtn}>+ Add dream</Link>
-}
-
-export default AddButton
