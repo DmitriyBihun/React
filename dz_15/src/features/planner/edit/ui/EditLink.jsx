9c7e5b6f538686e6799d75ed112039a39d0e663d
Dmytro Bihun <89932822+DmitriyBihun@users.noreply.github.com>
2026-02-02 12:01:16 +0200

    Delete dz_15 directory

--- dz_15/src/features/planner/edit/ui/EditLink.jsx
+++
@@ -1,8 +0,0 @@
-import { Link } from 'react-router'
-import style from './EditLink.module.css'
-
-function EditLink({ id }) {
-  return <Link to={`/planner/${id}/edit`} className={style.editLink}>Edit</Link>
-}
-
-export default EditLink
